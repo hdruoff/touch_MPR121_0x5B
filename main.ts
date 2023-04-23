81aaae3f1a2de1c7f82d8fa506533049087bca81
ha-dida <hdruoff@gmx.de>
2023-04-23 18:22:49 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+mpr121.onTouchSensorTouched(TouchSensor.T0, function () {
+    basic.setLedColor(0xff0000)
+})
+mp3touch.init(
+)
+basic.showNumber(mp3touch.touchedFeeler())
 basic.forever(function () {
 	
 })
